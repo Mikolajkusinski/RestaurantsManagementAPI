dd053093bcb8ddd2e9aa0c8ca7f9b8fba4e23cd5
MikolajKusinski <mikolajk558@gmail.com>
2024-08-19 20:46:46 +0200

    Entity Framework configuration
    Added Entity Framework;
    Added Entities for Restaurant, Address and Dish;
    Added migration;
    Configured DbContext;

--- RestaurantsManagement/Program.cs
+++ RestaurantsManagement/Program.cs
@@ -1,19 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure service
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RestaurantDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantDbConnectionString"))
+);
+builder.Services.AddScoped<RestaurantSeeder>();
 
 // Configure
 var app = builder.Build();
 
+using var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetService<RestaurantSeeder>();
+var dbContext = scope.ServiceProvider.GetService<RestaurantDbContext>();
+
+var pendingMigrations = dbContext.Database.GetPendingMigrations();
+if (pendingMigrations.Any())
+{
+    dbContext.Database.Migrate();
+}
+
+seeder.Seed();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API"));
+
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
